1af45de9eb8c42f15d32adafa80b280510ae06d8
Seokjun Kang <kangsj0327@gmail.com>
2024-04-12 00:29:28 +0900

    feat: 코스 편집 페이지에서 카카오 지도 띄우기 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import SeoulTrails from './pages/SeoulTrails';
 import Community from './pages/Community';
+import EditUserCourse from './pages/EditUserCourse';
+
 function App() {
   return (
     <>
@@ -17,6 +19,7 @@ function App() {
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/seoul-trails" element={<SeoulTrails />} />
           <Route path="/community" element={<Community />} />
+          <Route path="/EditUserCourse" element={<EditUserCourse />} />
         </Routes>
       </BrowserRouter>
     </>
