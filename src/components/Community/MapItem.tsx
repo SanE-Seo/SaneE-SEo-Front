e75d0ac3a9baeab58a41aaab12baba8af6083983
ej070961 <ej070961@gmail.com>
2024-04-25 16:52:36 +0900

    feat: 카카오맵 연동 및 지역 행정구동 검색 기능 구현 (#21)

--- src/components/Community/MapItem.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function MapItem() {
-  return <div>MapItem</div>;
-}
-
-export default MapItem;
--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -3,19 +3,36 @@ import DefaultLayout from '../components/DefaultLayout';
 import * as C from '../styles/community.style';
 import RightArrowIcon from '../assets/icons/right-arrow';
 import { ReactComponent as SearchIcon } from '../assets/icons/search-icon.svg';
-import MapItem from '../components/Community/MapItem';
 import { useNavigate } from 'react-router-dom';
+import UserTrailMap from '../components/Community/UserTrailMap';
+import PlaceSearchModal from '../components/Community/PlaceSearchModal';
+import { useLocation } from '../contexts/LocationContext';
 function Community() {
   const [placeInput, setPlaceInput] = useState<string>('');
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const { latitude, longitude } = useLocation();
+
+  const [lat, setLat] = useState<number>(latitude);
+  const [long, setLong] = useState<number>(longitude);
 
   const navigate = useNavigate();
+
   return (
     <>
       <DefaultLayout />
       <C.HeaderLayout>
         <C.SearchContainer>
           <SearchIcon />
-          <input name="text" placeholder="지역을 검색하세요"></input>
+          <input
+            name="text"
+            placeholder="지역을 검색하세요"
+            value={placeInput}
+            onChange={(e) => {
+              setPlaceInput(e.target.value);
+              setIsOpen(true);
+            }}
+          ></input>
         </C.SearchContainer>
         <C.AddButton onClick={() => navigate('/edit-user-course')}>
           <span className="button_icon-wrapper">
@@ -25,7 +42,17 @@ function Community() {
           <span>내 코스 추가하기</span>
         </C.AddButton>
       </C.HeaderLayout>
-      <MapItem />
+      {isOpen && (
+        <PlaceSearchModal
+          placeInput={placeInput}
+          setPlaceInput={setPlaceInput}
+          setIsOpen={setIsOpen}
+          setLat={setLat}
+          setLong={setLong}
+        />
+      )}
+
+      <UserTrailMap lat={lat} long={long} />
     </>
   );
 }
