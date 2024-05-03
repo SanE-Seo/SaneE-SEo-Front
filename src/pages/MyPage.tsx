1342b419c3ef915e522ca5dd41fbfb01ab322401
Seokjun Kang <kangsj0327@gmail.com>
2024-05-04 00:07:22 +0900

    feat: 마이페이지 사용자 정보 레이아웃

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DefaultLayout from '../components/DefaultLayout';
+import * as M from '../styles/my-page.style';
+import DefaultProfileImg from '../assets/image/default-profile.png';
+import { ReactComponent as SettingIcon } from '../assets/icons/setting-icon.svg';
+
+import { useQuery } from '@tanstack/react-query';
+import { getUser, logoutUser } from '../apis/user';
+import { useAuth } from '../contexts/AuthContext';
 
 function MyPage() {
-  return <div>MyPage</div>;
+  const { isLoggedIn, logout } = useAuth();
+
+  const { isLoading, data } = useQuery({
+    queryKey: ['getUser'],
+    queryFn: () => getUser(),
+    enabled: isLoggedIn, //로그인한 상태에서만 실행
+  });
+
+  return (
+    <>
+      <DefaultLayout>
+        <M.MyPageWrapper>
+          <M.UserInfoLayout>
+            <div className="user-info-container">
+              <div className="user-image-container">
+                {isLoggedIn && !isLoading && data ? (
+                  <img
+                    src={data.image == null ? DefaultProfileImg : data.image}
+                    alt="profile"
+                    className="profile-icon"
+                  />
+                ) : (
+                  <img src={DefaultProfileImg}></img>
+                )}
+              </div>
+              <div className="user-name-container">
+                {isLoggedIn && !isLoading && data?.nickname
+                  ? data.nickname
+                  : '이름 없음'}
+              </div>
+              <div className="user-email-container">
+                {isLoggedIn && !isLoading && data?.email
+                  ? data.email
+                  : 'example@gmail.com'}
+              </div>
+            </div>
+            <M.UserInfoEditButton>
+              {/* onClick={() => setIsOpenMenu(!isOpenMenu)} */}
+              프로필 관리
+              <SettingIcon />
+            </M.UserInfoEditButton>
+          </M.UserInfoLayout>
+        </M.MyPageWrapper>
+      </DefaultLayout>
+    </>
+  );
 }
 
 export default MyPage;
---
+++ src/styles/my-page.style.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const MyPageWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: ${(props) => props.theme.colors.background};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const UserInfoLayout = styled.div`
+  width: 260px;
+  height: 300px;
+  margin: auto;
+
+  background: #ffffff;
+  box-shadow:
+    -2.17893px -2.17893px 6.5368px #ffffff,
+    2.17893px 2.17893px 6.5368px rgba(174, 174, 192, 0.4);
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .user-info-container {
+    margin-top: 48px;
+    .user-image-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .profile-icon {
+        width: 95px;
+        height: 95px;
+        border-radius: 50%;
+        margin: 5px;
+      }
+    }
+
+    .user-name-container {
+      ${(props) => props.theme.fonts.title_md};
+      color: black;
+      margin: 10px;
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .user-email-container {
+      ${(props) => props.theme.fonts.text_lg};
+      color: ${(props) => props.theme.colors.gray500};
+      align-items: center;
+      margin: 10px;
+      display: flex;
+      flex-direction: column;
+    }
+  }
+`;
+
+export const UserInfoEditButton = styled.button`
+  ${(props) => props.theme.fonts.title_sm};
+  color: ${(props) => props.theme.colors.green500};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 15px;
+  margin-left: auto;
+  padding: 5px;
+  svg {
+    margin-left: 5px; // Adds space between text and icon
+  }
+  cursor: pointer;
+`;
+
+export const UserActivityLayout = styled.div`
+  width: 884;
+  height: 670px;
+  margin: auto;
+`;
